a383b682efbcd39b96663eab897e322c983a2ffb
shafawatihsaleh <shafawatih7012@gmail.com>
2023-03-30 15:53:04 +0800

    messagse

---
+++ quiz1.js
@@ -0,0 +1,48 @@
+//let message = "benr"
+//console.log(message,typeof message)
+
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+
+
+
+
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+//console.log(data[3])
+
+
+let user = {
+    name : "shafa",
+    age : 22,
+    faculty : "FKEKK"
+}
+//console.log(user.name)
+//console.log(user.age)
+//console.log(user.faculty)
+
+let message = [
+    {
+        name : "user1" ,
+        date : "16/7/2023",
+        msg  : "good job",
+        like : 1200
+    },
+    {
+        name : "user2" ,
+        date : "16/7/2023",
+        msg  : "very good job",
+        like : 2200
+    }
+    
+]
+console.log(message[1].msg)  //>>very good job    
+
+
